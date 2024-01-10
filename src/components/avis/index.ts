869159bee9bd6ce2768046445574c7d262bee9be
BrightkyEfoo <brightefoo@gmail.com>
2024-01-10 13:13:11 +0100

    export all things

---
+++ src/components/avis/index.ts
@@ -0,0 +1,7 @@
+export * from './avis.controller'
+export * from './avis.model'
+export * from './avis.routes'
+export * from './avis.zod'
+export * from './avis.types'
+export * from './avis.schemas'      
+export * from './avis.services'      
