1e686eee285fffd9f358def71be5f2261c2035b5
BrightkyEfoo <brightefoo@gmail.com>
2024-01-10 13:12:32 +0100

    add send user mail and store avis to db

--- src/components/avis/avis.controller.ts
+++ src/components/avis/avis.controller.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
 import { avisService } from './avis.services';
+import { readFileSync } from 'fs';
+import path from 'path';
+import mailingService from '@components/mailing/mailing.service';
+
+const confirmationTemplate = readFileSync(
+  path.join(__dirname, '../../views/avis/template-mail-to-user.hbs'),
+  'utf-8'
+);
 
 const giveAvis = async (req: Request, res: Response) => {
-  const { message, email, impression } = req.body;
+  const { message, email, impression, subject } = req.body;
   try {
     // store avis to db
     const avis = await avisService.createAvis({
+      subject,
       message,
       email,
       impression,
@@ -14,6 +23,14 @@ const giveAvis = async (req: Request, res: Response) => {
     // store avis to cms
 
     // send mail to user
+    const template = Handlebars.compile(confirmationTemplate);
+    mailingService.sendWithNodemailer({
+      to: email,
+      html: template({ subject }),
+      subject: 'Votre avis a ete recu',
+    });
+
+    return res.json({ msg: 'success', avis });
   } catch (err) {
     res.status(500).json({ msg: 'quelque chose a mal tourne' });
   }
