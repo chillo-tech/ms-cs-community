bd158100198a3d17fdb01f34a8ffa15e4919c571
BrightkyEfoo <brightefoo@gmail.com>
2024-01-24 18:17:06 +0100

    delete unused files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-env node */
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.json');
-
-module.exports = {
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' } ),
-};
