c11cd166da5d22b0253ba4144f1815736534e666
Achille <achille.mbougueng@chillo.tech>
2024-01-24 10:53:47 +0100

    fusion de code

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,6 @@ app.listen(port, async () => {
   console.log(`Server listening on port ${port}`);
   const res = await dbInit();
   if (res) console.log('succesfully connected to mongodb');
-
-  console.log(`----------------------`);
-  console.log(JSON.stringify(process.env, null, 2));
-  console.log(`----------------------`);
 });
 
 process.on('unhandledRejection', err => {
